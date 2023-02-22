e5d6727bda955dfceae983ca146cfc3508e8db14
Kamil Kirstein <Kamilkirstein@googlemail.com>
2023-02-22 13:34:51 +0100

    Completed Chapter 1

---
+++ app/src/main/java/com/kamilkirstein/goequiz/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.kamilkirstein.goequiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton : Button
+    private lateinit var falseButton : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        // set Listeners:
+        trueButton.setOnClickListener { view : View ->
+            // do something
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        falseButton.setOnClickListener { view : View ->
+            //do something
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
